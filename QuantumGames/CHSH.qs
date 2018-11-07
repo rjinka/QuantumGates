5fca2d6927c41c741856b18224d766635064ab7b
Rama Krishna Jinka <jinkaramakrishna@hotmail.com>
2018-11-06 20:50:19 -0500

    Add files via upload

---
+++ QuantumGames/CHSH.qs
@@ -0,0 +1,145 @@
+﻿namespace QuantumGames
+{
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Extensions.Math;
+    open Microsoft.Quantum.Primitive;
+
+   
+
+    operation MeasureAliceQbit(bit : Bool, qubit : Qubit) : (Result)
+    {
+        body
+        {
+            if (bit)
+            {
+                // Measure in sign basis if bit is 1
+                return Measure([PauliX], [qubit]);
+            }
+            else
+            {
+                // Measure in computational basis if bit is 0
+                return Measure([PauliZ], [qubit]);
+            }
+        }
+    }
+
+    operation MeasureBobQbit(bit : Bool, qubit : Qubit) : (Result)
+    {
+        body
+        {
+            if (bit)
+            {
+                // Measure in -π/8 basis if bit is 1
+                let rotation = 2.0 * PI() / 8.0;
+                Ry(rotation, qubit);
+                return M(qubit);
+            }
+            else
+            {
+                // Measure in π/8 basis if bit is 0
+                let rotation = -2.0 * PI() / 8.0;
+                Ry(rotation, qubit);
+                return M(qubit);
+            }
+        }
+    }
+
+	 operation MeasureJohnQbit(bit : Bool, qubit : Qubit) : (Result)
+    {
+        body
+        {
+            if (bit)
+            {
+                // Measure in sign basis if bit is 1
+                return Measure([PauliY], [qubit]);
+            }
+            else
+            {
+                // Measure in computational basis if bit is 0
+                return Measure([PauliZ], [qubit]);
+            }
+        }
+    }
+
+	 operation MeasureMikeQbit(bit : Bool, qubit : Qubit) : (Result)
+    {
+        body
+        {
+            if (bit)
+            {
+                // Measure in -π/8 basis if bit is 1
+                let rotation = 2.0 * PI() / 8.0;
+                Ry(rotation, qubit);
+                return M(qubit);
+            }
+            else
+            {
+                // Measure in π/8 basis if bit is 0
+                let rotation = -2.0 * PI() / 8.0;
+                Ry(rotation, qubit);
+                return M(qubit);
+            }
+        }
+    }
+
+
+    operation PlayQuantumStrategy(
+        aliceBit : Bool,
+        bobBit : Bool,
+		johnBit : Bool,	
+		mikeBit : Bool,
+        aliceMeasuresFirst : Bool)
+        :Bool[]
+    {
+        body
+        {
+            mutable aliceResult = Zero;
+            mutable bobResult = Zero;
+			mutable JohnResult = Zero;
+			mutable mikeResult = Zero;
+			mutable intersult = Zero;
+			mutable resultArray = new Bool[2];
+
+            using (qubits = Qubit[4])
+            {
+                // Alice and Bob get one qubit each
+                let aliceQbit = qubits[0];
+                let bobQbit = qubits[1];
+				let JohnQbit = qubits[2];
+				let mikeQbit = qubits[3];
+                // Entangle Alice & Bob's qubits
+                H(aliceQbit);
+                CNOT(aliceQbit, bobQbit);
+
+				// Entangle john and mike
+                H(JohnQbit);
+                CNOT(JohnQbit, mikeQbit);
+
+                // Randomize who measures first
+                if (aliceMeasuresFirst)
+                {
+                    set aliceResult = MeasureAliceQbit(aliceBit, aliceQbit);
+                    set bobResult = MeasureBobQbit(bobBit, bobQbit);
+					set JohnResult = MeasureJohnQbit(johnBit,JohnQbit);
+					set mikeResult = MeasureMikeQbit(mikeBit,mikeQbit);
+					
+                }
+                else
+                {
+                    set bobResult = MeasureBobQbit(bobBit, bobQbit);
+                    set aliceResult = MeasureAliceQbit(aliceBit, aliceQbit);
+					set JohnResult = MeasureJohnQbit(johnBit,JohnQbit);
+					set mikeResult = MeasureMikeQbit(mikeBit,mikeQbit);
+                }
+
+                ResetAll(qubits);
+            }
+
+			set resultArray[0] = aliceResult == bobResult;
+			set resultArray[1] = JohnResult == mikeResult;
+			
+
+            return resultArray;
+        }
+    }
+}
--- QuantumGames/Program.cs
+++ QuantumGames/Program.cs
@@ -9,56 +9,101 @@ namespace QuantumGames
     {
         static void Main(string[] args)
         {
-            const int trialCount = 10000;
-            Random generator = new Random();
-
-            using (QuantumSimulator sim = new QuantumSimulator())
+            int j = 0;
+            do
             {
-                int classicalWinCount = 0;
-                int quantumWinCount = 0;
-                for (int i = 0; i < trialCount; i++)
-                {
-                    bool aliceBit = GetRandomBit(generator);
-                    bool bobBit = GetRandomBit(generator);
-                    bool johnBit = GetRandomBit(generator);                    
-                    bool aliceMeasuresFirst = GetRandomBit(generator);
-                    bool classicalXor =
-                        !PlayClassicalStrategy(aliceBit, bobBit, johnBit);                   
-                    bool quantumXor =
-                        !PlayQuantumStrategy.Run(
-                            sim,
-                            aliceBit,
-                            bobBit,
-                            johnBit,                           
-                            aliceMeasuresFirst).Result;
+                j++;
+                const int trialCount = 20000;
+                Random generator = new Random();
 
-                    if ((aliceBit && bobBit && johnBit) == classicalXor)
+                using (QuantumSimulator sim = new QuantumSimulator())
+                {
+                    int classicalWinCount = 0;
+                    int quantumWinCount = 0;
+                    int classicalAliceBobWinCount = 0;
+                    int classicalJohnMikeWinCount = 0;
+                    int quantumAliceBobWinCount = 0;
+                    int quantumJohnMikeWinCount = 0;
+                    int classicalnowincount = 0;
+                    int quantumnowincount = 0;
+                    for (int i = 0; i < trialCount; i++)
                     {
-                        classicalWinCount++;
+                        bool aliceBit = GetRandomBit(generator);
+                        bool bobBit = GetRandomBit(generator);
+                        bool johnBit = GetRandomBit(generator);
+                        bool mikeBit = GetRandomBit(generator);
+                        bool aliceMeasuresFirst = GetRandomBit(generator);
+                        bool[] classicalXor =
+                            PlayClassicalStrategy(aliceBit, bobBit, johnBit, mikeBit);
+                        bool[] quantumXor =
+                            PlayQuantumStrategy.Run(
+                                sim,
+                                aliceBit,
+                                bobBit,
+                                johnBit,
+                                mikeBit,
+                                aliceMeasuresFirst).Result.ToArray();
+
+                        if ((aliceBit && bobBit) == !classicalXor[0])
+                        {
+                            classicalWinCount++;
+                            classicalAliceBobWinCount++;
+                        }
+                        else if ((johnBit && mikeBit) == !classicalXor[1])
+                        {
+                            classicalWinCount++;
+                            classicalJohnMikeWinCount++;
+                        }
+                        else
+                        {
+                            classicalnowincount++;
+                        }
+                        if ((aliceBit && bobBit) == !quantumXor[0])
+                        {
+                            quantumWinCount++;
+                            quantumAliceBobWinCount++;
+                        }
+                        else if ((johnBit && mikeBit) == !quantumXor[1])
+                        {
+                            quantumWinCount++;
+                            quantumJohnMikeWinCount++;
+                        }
+                        else
+                        {
+                            quantumnowincount++;
+                        }
                     }
-                    if ((aliceBit && bobBit && johnBit) == quantumXor)
+
+                    Console.WriteLine(
+                        "Classical success rate: "
+                        + classicalWinCount / (float)trialCount);
+                    Console.WriteLine("Classical Alice and Bob won {0} times ", classicalAliceBobWinCount);
+                    Console.WriteLine("Classical John and Mike won {0} times ", classicalJohnMikeWinCount);
+                    Console.WriteLine("Classical did not win {0} times ", classicalnowincount);
+                    Console.WriteLine();
+                    Console.WriteLine(
+                        "Quantum success rate: "
+                        + quantumWinCount / (float)trialCount);
+                    Console.WriteLine("Quantum Alice and Bob won {0} times ", quantumAliceBobWinCount);
+                    Console.WriteLine("Quantum John and Mike won {0} times ", quantumJohnMikeWinCount);
+                    Console.WriteLine("Quantum did not win {0} times ", quantumnowincount);
+                    Console.WriteLine();
+
+                    if (quantumWinCount > classicalWinCount)
                     {
-                        quantumWinCount++;
+                        Console.WriteLine("Round {0} Quantum Win ",j);
                     }
-                }
+                    else
+                    {
+                        Console.WriteLine("Round {0} Classical Win ", j);
+                    }
 
-                Console.WriteLine(
-                    "Classical success rate: "
-                    + classicalWinCount / (float)trialCount);
-                Console.WriteLine(
-                    "Quantum success rate: "
-                    + quantumWinCount / (float)trialCount);
-
-                if (quantumWinCount > classicalWinCount)
-                {
-                    Console.WriteLine("Quantum Win");
                 }
-                else
-                {
-                    Console.WriteLine("Classical Win");
-                }
-                Console.ReadLine();
-            }
+                Console.WriteLine();
+            } while (j < 10);
+            Console.WriteLine();
+            Console.WriteLine("All rounds completed");
+            Console.ReadLine();
         }
 
         /// <summary>
@@ -80,21 +125,18 @@ namespace QuantumGames
         /// <param name="aliceBit">The bit given to Alice (X).</param>
         /// <param name="bobBit">The bit given to Bob (Y).</param>
         /// <returns>Whether Alice and Bob's output bits match.</returns>
-        private static bool PlayClassicalStrategy(bool aliceBit, bool bobBit, bool johnBit)
+        private static bool[] PlayClassicalStrategy(bool aliceBit, bool bobBit, bool johnBit,bool mikeBit)
         {
             bool aliceOutput = false;
             bool bobOutput = false;
             bool johnOutput = false;
-            //return aliceOutput == bobOutput;
+            bool mikeOutput = false;
+            bool[] result = new bool[2];
+            result[0] = aliceOutput == bobOutput;
+            result[1] = johnOutput == mikeOutput;
+            return result;
 
-            if (aliceOutput == bobOutput)
-            {
-                return aliceOutput == johnOutput;
-            }            
-			else
-			{
-                return bobOutput == johnOutput;
-            }
+
         }
     }
 }
